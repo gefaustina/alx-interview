b5be090bc76c142bcfdaca7a2394ba313274e05c
gefaustina <famoust4luv@yahoo.com>
2023-08-04 13:38:30 +0000

    updates

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle.
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
-Example:
-    $ ./0-nqueens.py N
-N must be an integer greater than or equal to 4.
-Attributes:
-    board (list): A list of lists representing the chessboard.
-    solutions (list): A list of lists containing solutions.
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
-"""
 
 
 import sys
@@ -23,11 +10,15 @@ def init_board(n):
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
     return (board)
+
+
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
+
+
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
     solution = []
@@ -37,6 +28,8 @@ def get_solution(board):
                 solution.append([r, c])
                 break
     return (solution)
+
+
 def xout(board, row, col):
     """X out spots on a chessboard.
     All spots where non-attacking queens can no
@@ -86,6 +79,8 @@ def xout(board, row, col):
             break
         board[r][c] = "x"
         c -= 1
+
+
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
     Args:
@@ -99,6 +94,7 @@ def recursive_solve(board, row, queens, solutions):
     if queens == len(board):
         solutions.append(get_solution(board))
         return (solutions)
+
     for c in range(len(board)):
         if board[row][c] == " ":
             tmp_board = board_deepcopy(board)
@@ -106,7 +102,10 @@ def recursive_solve(board, row, queens, solutions):
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
                                         queens + 1, solutions)
+
     return (solutions)
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
@@ -117,6 +116,7 @@ if __name__ == "__main__":
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
+
     board = init_board(int(sys.argv[1]))
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
